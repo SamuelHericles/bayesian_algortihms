26f90003759d224bdaab2b1289f1a2b25eac78bf
samuelsilveira <samuel.herilces@alu.ufc.br>
2021-03-21 22:59:13 -0300

    Update link dataset

--- classes/LoaderFile.py
+++ classes/LoaderFile.py
@@ -3,7 +3,7 @@ import pandas as pd
 class Loader:
         
         def __init__(self):
-            path = 'https://raw.githubusercontent.com/SamuelHericles/Algoritmos_de_classificao_baysianos/master/dema_dados.csv'
+            path = 'https://raw.githubusercontent.com/SamuelHericles/Algoritmos_de_classificao_baysianos/master/data/dema_dados.csv'
             self.data_derma = pd.read_csv(path)
             self.data_derma.sort_values('c35',inplace=True)
             self.data_derma.reset_index(drop=True,inplace=True)
